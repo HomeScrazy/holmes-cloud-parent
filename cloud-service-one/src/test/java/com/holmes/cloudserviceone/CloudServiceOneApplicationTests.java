66303887cb86fb421dae48e06b799cebcb473fbc
Holmes <crazy_runcheng@163.com>
2021-10-19 10:01:10 +0800

    project init

---
+++ cloud-service-one/src/test/java/com/holmes/cloudserviceone/CloudServiceOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holmes.cloudserviceone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudServiceOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
