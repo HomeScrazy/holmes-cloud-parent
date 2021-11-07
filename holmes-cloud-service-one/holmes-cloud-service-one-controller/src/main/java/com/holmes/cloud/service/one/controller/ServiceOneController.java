6f72702bef642fb09d4df604aace35bf979d865a
Holmes <crazy_runcheng@163.com>
2021-11-07 15:35:58 +0800

    配置文件完成

--- holmes-cloud-service-one/holmes-cloud-service-one-controller/src/main/java/com/holmes/cloud/service/one/controller/ServiceOneController.java
+++ holmes-cloud-service-one/holmes-cloud-service-one-controller/src/main/java/com/holmes/cloud/service/one/controller/ServiceOneController.java
@@ -1,14 +1,17 @@
 package com.holmes.cloud.service.one.controller;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @EnableAutoConfiguration
 @RequestMapping("service/one")
+@Slf4j
 public class ServiceOneController {
 
     @RequestMapping(value = "/echo", method = RequestMethod.GET)
     public String echo(@RequestParam("name") String name) {
+        log.info("print a simple log in service ont");
         return "Hello service one  " + name;
     }
 }
